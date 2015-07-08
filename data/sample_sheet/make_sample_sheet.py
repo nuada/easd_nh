8cde97ba8008d751339f6e1c120fc5bc3d09c9a3
Piotr Radkowski <piotr.radkowski@uj.edu.pl>
2015-07-08 17:20:13 +0200

    Simple unique IDs

--- data/sample_sheet/make_sample_sheet.py
+++ data/sample_sheet/make_sample_sheet.py
@@ -19,7 +19,14 @@ samples.column = samples.column.astype('int')
 samples.plate_column = samples.plate_column.astype('int')
 samples.sample_id = samples.sample_id.map(lambda s: s.strip().upper().replace(' ', ''))
 
+samples['unique_id'] = ''
+for sample, count in samples.sample_id.value_counts().iteritems():
+	if count > 1:
+		samples.loc[samples.sample_id == sample, 'unique_id'] = map(lambda x, y: '{}_{}'.format(x,y), samples[samples.sample_id == sample].sample_id, range(0, count))
+	else:
+		samples.loc[samples.sample_id == sample, 'unique_id'] = sample
+
 for idx, row in samples.sort(['run', 'array', 'row', 'column']).iterrows():
 	d = row.to_dict()
 	d['well'] = '{0}{1:02d}'.format(string.letters[d['row']-1].upper(), d['plate_column'])
-	print '{sample_id}.{array}_R{row:02d}C{column:02d},{sample_id},{run},{well},{array},R{row:02d}C{column:02d},,{run}-{plate},,,'.format(**d)
+	print '{unique_id},{sample_id},{run}_{plate},{well},{array},R{row:02d}C{column:02d},,,,,'.format(**d)
