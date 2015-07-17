25c94c6c5d90cfeaf0e27ad65f2e6e98e7b67974
Piotr Radkowski <piotr.radkowski@uj.edu.pl>
2015-07-17 16:43:54 +0200

    Basic filtering implemented

--- qc.R
+++ qc.R
@@ -1,37 +1,64 @@
-setwd("~/projekty/easd_nh")
-
-plink_path <- '/usr/bin/p-link'
-
+#' Configuration
+#+ echo=FALSE
+data_dir <- '/data/easd_nh'
+phenotype_dir <- paste(data_dir, 'phenotype', sep='/')
+genotype_dir <- paste(data_dir, 'genotype', sep='/')
 temp_dir = tempdir()
-data_dir = 'data'
 
+#' PLINK wrapper
+#+ echo=FALSE
+plink_path <- '/usr/bin/plink'
 plink <- function(..., infile, outfile = NULL){
   if (is.null(outfile)) {
-    outfile = tempfile(tmpdir = tmp)
+    outfile = tempfile(tmpdir = temp_dir)
   }
-  system(paste(plink_path, '--noweb', '--bfile', infile, '--make-bed', '--out', outfile, ...))
+  print(system(paste(plink_path, '--noweb', '--bfile', infile, '--make-bed', '--out', outfile, ...), intern = T))
   return(outfile)
 }
 
+to_file <- function(x) {
+  file_name <- tempfile(tmpdir = temp_dir)
+  write.table(x, file=file_name, col.names = F, row.names = F, quote = F)
+  return(file_name)
+}
+
 #' # Update GenomeStudio result with data from phentoype.csv
-phenotype <- read.csv('data/phenotype.csv', sep='\t')
+phenotype <- read.csv(paste(data_dir, 'phenotype.csv', sep='/'), sep='\t')
 
 #' Convert file exported from Genome studio to binary file
-gs_export <- tempfile(tmpdir = temp_dir)
-system(paste(plink_path, '--noweb', '--file', paste(c(data_dir, 'easd_nh', sep='/')), '--make-bed', '--out', gs_export))
+raw_genotypes <- tempfile(tmpdir = temp_dir)
+system(paste(plink_path, '--noweb', '--file', paste(genotype_dir, 'PLINK_090715_0107', 'easd_nh', sep='/'), '--make-bed', '--out', raw_genotypes))
 
 #' Update FIDs
-update_ids_file <- tempfile(tmpdir = temp_dir)
-write.table(subset(phenotype, select=c(GenomeStudio_FID, OMICRON_ID, FAMILY_ID, OMICRON_ID)), file=update_ids_file, col.names = F)
-updated_ids <- plink('--update-ids', update_ids_file, infile=gs_export)
+genotypes <- plink('--update-ids',
+                   to_file(subset(phenotype, select=c(GENOMESTUDIO_FID, OMICRON_ID, FAMILY_ID, OMICRON_ID))),
+                   infile=raw_genotypes)
 
 #' Update Affected and Sex fields
+genotypes <- plink('--update-sex',
+                   to_file(subset(phenotype, select=c(FAMILY_ID, OMICRON_ID, SEX_PLINK))),
+                   infile=genotypes)
 
 #' # Filtering
-#' Drop individuals excluded individuals, eg. NEWBORN-*
+#' Drop excluded individuals (not included in phentoype file)
+genotypes <- plink('--keep',
+                   to_file(subset(phenotype, select=c(FAMILY_ID, OMICRON_ID))),
+                   infile=genotypes)
 
-#' MAF filter
-#' CR filter
-#' HWE filter
+#' MAF fitering
+genotypes <- plink('--maf', '0.01', infile=genotypes)
 
-#' Remove tmp dir when done?!?
+#' Missingnes per marker
+genotypes <- plink('--geno', '0.1', infile=genotypes)
+
+#' HWE filtering
+# TODO record - do not remove
+genotypes <- plink('--hwe', '1e-5', infile=genotypes)
+
+#' Missingnes per subject
+genotypes <- plink('--mind', '0.02', infile=genotypes)
+
+#' Sex check
+genotypes <- plink('--check-sex', infile=genotypes)
+
+# TODO Remove tmp dir when done?!?
